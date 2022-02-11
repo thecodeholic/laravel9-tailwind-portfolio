a0997544efc147e8b07201efffa6171a2068ce30
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-02-11 10:28:35 +0400

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './storage/framework/views/*.php',
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
