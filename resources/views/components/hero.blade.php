5bcc290979e381de5fe123bb72d3af92d84796f3
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-02-18 10:59:13 +0400

    Add Hero component

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,122 @@
+<!-- ====== Hero Section Start -->
+<div class="relative pt-[60px] lg:pt-[80px] pb-[110px] bg-white">
+    <div class="container">
+        <div class="flex flex-wrap -mx-4">
+            <div class="w-full lg:w-5/12 px-4">
+                <div class="hero-content">
+                    <h1
+                        class="text-dark font-bold text-4xl sm:text-[42px] lg:text-[40px] xl:text-[42px] leading-snug mb-3"
+                    >
+                        My Name is Zura <br/>
+                        and I am <br/>
+                        TheCodeholic.
+                    </h1>
+                    <p class="text-base mb-8 text-body-color max-w-[480px]">
+                        Building things is what matters most. <br>
+                        Learning by building is what gives you most. <br>
+                        So, go ahead and build something cool...
+                    </p>
+                    <ul class="flex flex-wrap items-center">
+                        <li>
+                            <a
+                                href="javascript:void(0)"
+                                class="
+                                  py-4
+                                  px-6
+                                  sm:px-10
+                                  lg:px-8
+                                  xl:px-10
+                                  inline-flex
+                                  items-center
+                                  justify-center
+                                  text-center text-white text-base
+                                  bg-primary
+                                  hover:bg-opacity-90
+                                  font-normal
+                                  rounded-lg
+                                "
+                            >
+                                Get Started
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="javascript:void(0)"
+                                class="
+                                  py-4
+                                  px-6
+                                  sm:px-10
+                                  lg:px-8
+                                  xl:px-10
+                                  inline-flex
+                                  items-center
+                                  justify-center
+                                  text-center text-base
+                                  font-normal
+                                  text-body-color
+                                  hover:text-primary
+                                "
+                            >
+                                <span class="mr-2">
+                                  <svg
+                                      width="22"
+                                      height="22"
+                                      viewBox="0 0 22 22"
+                                      fill="none"
+                                      xmlns="http://www.w3.org/2000/svg"
+                                  >
+                                    <circle cx="11" cy="11" r="11" fill="#3056D3"/>
+                                    <rect
+                                        x="6.90906"
+                                        y="13.3636"
+                                        width="8.18182"
+                                        height="1.63636"
+                                        fill="white"
+                                    />
+                                    <rect
+                                        x="10.1818"
+                                        y="6"
+                                        width="1.63636"
+                                        height="4.09091"
+                                        fill="white"
+                                    />
+                                    <path
+                                        d="M11 12.5454L13.8343 9.47726H8.16576L11 12.5454Z"
+                                        fill="white"
+                                    />
+                                  </svg>
+                                </span>
+                                Check what I built previously
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="clients pt-16">
+                        <h6
+                            class="
+                    font-normal
+                    text-xs
+                    flex
+                    items-center
+                    text-body-color
+                    mb-2
+                  "
+                        >
+                            Follow me on social media
+                            <span
+                                class="w-8 h-[1px] bg-body-color inline-block ml-2"
+                            ></span>
+                        </h6>
+                        <div class="flex items-center">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="hidden lg:block lg:w-1/12 px-4"></div>
+            <div class="w-full lg:w-6/12 px-4">
+                <div class="lg:text-right lg:ml-auto">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ====== Hero Section End -->
