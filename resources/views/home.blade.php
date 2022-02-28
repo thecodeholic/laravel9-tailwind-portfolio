03a44f3ad2485d4226b102c8606d24030e7de2ea
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-02-28 22:31:05 +0400

    Change Github and YouTube button colors

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,14 +14,14 @@
         </x-slot>
         <a
             href="javascript:void(0)"
-            class=" inline-block py-4 px-6 md:px-9 lg:px-6 xl:px-9 rounded text-base font-medium bg-white bg-opacity-[15%] text-white hover:bg-opacity-100 hover:text-primary mr-4 my-1 transition "
+            class=" inline-block py-4 px-6 md:px-9 lg:px-6 xl:px-9 rounded text-base font-medium bg-gray-900 hover:bg-gray-800 text-white mr-4 my-1 transition "
         >
 
             View on Github
         </a>
         <a
             href="javascript:void(0)"
-            class=" inline-block py-4 px-6 md:px-9 lg:px-6 xl:px-9 rounded text-base font-medium bg-[#13C296] transition hover:bg-opacity-90 text-white my-1 "
+            class=" inline-block py-4 px-6 md:px-9 lg:px-6 xl:px-9 rounded text-base font-medium bg-red-700 transition hover:bg-opacity-90 text-white my-1 "
         >
             Watch on YouTube
         </a>
