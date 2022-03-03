5b3e363e6b9c37d039ae6415c8b2a59a40ed9446
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-03-04 00:19:41 +0400

    Write texts for about section

--- resources/views/components/about-section.blade.php
+++ resources/views/components/about-section.blade.php
@@ -1,79 +1,67 @@
 <!-- ====== About Section Start -->
 <section class="dark:bg-slate-800 pt-20 lg:pt-[120px] pb-12 lg:pb-[90px] overflow-hidden">
-    <div class="container">
-        <div class="flex flex-wrap justify-between items-center -mx-4">
-            <div class="w-full lg:w-6/12 px-4">
-                <div class="flex items-center -mx-3 sm:-mx-4">
-                    <div class="w-full xl:w-1/2 px-3 sm:px-4">
-                        <div class="py-3 sm:py-4">
-                            <img
-                                src="https://cdn.tailgrids.com/1.0/assets/images/services/image-1.jpg"
-                                alt=""
-                                class="rounded-2xl w-full"
-                            />
-                        </div>
-                        <div class="py-3 sm:py-4">
-                            <img
-                                src="https://cdn.tailgrids.com/1.0/assets/images/services/image-2.jpg"
-                                alt=""
-                                class="rounded-2xl w-full"
-                            />
-                        </div>
-                    </div>
-                    <div class="w-full xl:w-1/2 px-3 sm:px-4">
-                        <div class="my-4 relative z-10">
-                            <img
-                                src="https://cdn.tailgrids.com/1.0/assets/images/services/image-3.jpg"
-                                alt=""
-                                class="rounded-2xl w-full"
-                            />
-                            <x-about.dots></x-about.dots>
-                        </div>
-                    </div>
-                </div>
+  <div class="container">
+    <div class="flex flex-wrap justify-between items-center -mx-4">
+      <div class="w-full lg:w-6/12 px-4">
+        <div class="flex items-center -mx-3 sm:-mx-4">
+          <div class="w-full xl:w-1/2 px-3 sm:px-4">
+            <div class="py-3 sm:py-4">
+              <img
+                src="https://cdn.tailgrids.com/1.0/assets/images/services/image-1.jpg"
+                alt=""
+                class="rounded-2xl w-full"
+              />
             </div>
-            <div class="w-full lg:w-1/2 xl:w-5/12 px-4">
-                <div class="mt-10 lg:mt-0">
-               <span class="font-semibold text-lg text-primary mb-2 block">
-               <blockquote class="text-sm text-gray-500 italic py-2 px-3 border-l-4 border-amber-500">
-                   "Everything is achievable with hard working"
-               </blockquote>
-               </span>
-                    <h2 class="font-bold text-3xl sm:text-4xl dark:text-gray-200 mb-8">
-                        About me
-                    </h2>
-                    <p class="text-base dark:text-gray-400 mb-8">
-                        It is a long established fact that a reader will be distracted
-                        by the readable content of a page when looking at its layout.
-                        The point of using Lorem Ipsum is that it has a more-or-less.
-                    </p>
-                    <p class="text-base  dark:text-gray-400 mb-12">
-                        A domain name is one of the first steps to establishing your
-                        brand. Secure a consistent brand image with a domain name that
-                        matches your business.
-                    </p>
-                    <a
-                        href="javascript:void(0)"
-                        class="
-                          py-4
-                          px-10
-                          lg:px-8
-                          xl:px-10
-                          inline-flex
-                          items-center
-                          justify-center
-                          text-center text-white text-base
-                          bg-primary
-                          hover:bg-opacity-90
-                          font-normal
-                          rounded-lg
-                          "
-                    >
-                        Get Started
-                    </a>
-                </div>
+            <div class="py-3 sm:py-4">
+              <img
+                src="https://cdn.tailgrids.com/1.0/assets/images/services/image-2.jpg"
+                alt=""
+                class="rounded-2xl w-full"
+              />
             </div>
+          </div>
+          <div class="w-full xl:w-1/2 px-3 sm:px-4">
+            <div class="my-4 relative z-10">
+              <img
+                src="https://cdn.tailgrids.com/1.0/assets/images/services/image-3.jpg"
+                alt=""
+                class="rounded-2xl w-full"
+              />
+              <x-about.dots></x-about.dots>
+            </div>
+          </div>
         </div>
+      </div>
+      <div class="w-full lg:w-1/2 xl:w-5/12 px-4">
+        <div class="mt-10 lg:mt-0">
+          <span class="font-semibold text-lg text-primary mb-2 block">
+             <blockquote class="text-sm text-gray-500 italic py-2 px-3 border-l-4 border-amber-500">
+                 "Everything is achievable with hard work"
+             </blockquote>
+          </span>
+          <h2 class="font-bold text-3xl sm:text-4xl dark:text-gray-200 mb-8">
+            About Me
+          </h2>
+          <p class="text-base dark:text-gray-400 mb-8">
+            I have been working as a professional software developer for 10+ years. <br />
+            During my career I have worked on various types of projects with various technologies,
+            but very often I feel that I am a junior developer who just started coding. <br />
+          </p>
+          <p class="text-base dark:text-gray-400 mb-8">
+            In 2018 I decided to create a YouTube channel and share my knowledge. My channel is mostly focused on
+            <span class="text-amber-500 font-bold">PHP</span> and <span class="text-amber-500 font-bold">JavaScript</span>. <br />
+            My content is mostly focused on practical examples and projects. <br />
+          </p>
+          <a
+            href="https://youtube.com/thecodeholic"
+            target="_blank"
+            class=" inline-block py-4 px-6 md:px-9 lg:px-6 xl:px-9 rounded text-base font-medium bg-red-700 transition hover:bg-opacity-90 text-white my-1 "
+          >
+              View my channel
+          </a>
+        </div>
+      </div>
     </div>
+  </div>
 </section>
 <!-- ====== About Section End -->
