51bc82565dfb9903863edd6aab9d8d68c524d8a6
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-03-04 00:21:25 +0400

    Add Github and Youtube links for PHP developer roadmap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,14 +13,15 @@
             </h2>
         </x-slot>
         <a
-            href="javascript:void(0)"
+            href="https://github.com/thecodeholic/php-developer-roadmap"
+            target="_blank"
             class=" inline-block py-4 px-6 md:px-9 lg:px-6 xl:px-9 rounded text-base font-medium bg-gray-900 hover:bg-gray-800 text-white mr-4 my-1 transition "
         >
-
             View on Github
         </a>
         <a
-            href="javascript:void(0)"
+            href="https://youtu.be/r9ndOH0tyfA"
+            target="_blank"
             class=" inline-block py-4 px-6 md:px-9 lg:px-6 xl:px-9 rounded text-base font-medium bg-red-700 transition hover:bg-opacity-90 text-white my-1 "
         >
             Watch on YouTube
