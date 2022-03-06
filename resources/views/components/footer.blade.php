7429ce2c27d55b1e1e845bb4712533868933dba5
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-03-06 15:36:39 +0400

    Create footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,37 +1,15 @@
 <!-- ====== Footer Section Start -->
-<footer class="bg-white dark:bg-slate-800 pt-20 lg:pt-[120px] pb-10 lg:pb-20 relative z-10">
+<footer class="bg-white dark:bg-slate-800 py-8 relative z-10">
   <div class="container">
-    <div class="flex flex-wrap -mx-4">
-      <div class="w-full sm:w-2/3 lg:w-3/12 px-4">
-        <div class="w-full mb-10">
-          <a
-            href="javascript:void(0)"
-            class="inline-flex items-center max-w-[160px] mb-6 text-3xl"
-          >
-            <img
-              src="{{ url('/img/logo.png') }}"
-              alt="logo"
-              class="max-w-full w-[80px] inline-block dark:hidden"
-            />
-            <img
-              src="{{ url('/img/logo-white.png') }}"
-              alt="logo"
-              class="max-w-full w-[80px] hidden dark:inline-block"
-            />
-            TheCodeholic
-          </a>
-        </div>
-      </div>
-      <div class="w-full sm:w-1/2 lg:w-3/12 px-4">
-        <div class="w-full mb-10">
-          <h4 class="text-dark text-lg font-semibold mb-9">Follow TheCodeholic On</h4>
-            <x-social-icons></x-social-icons>
-        </div>
-      </div>
+    <div class="flex flex-wrap justify-between">
+      <span class="inline-flex items-center text-xl">
+        Follow me on Social Media
+      </span>
+      <x-social-icons class="mb-0"></x-social-icons>
     </div>
   </div>
   <div>
-      <span class="absolute left-0 bottom-0 z-[-1]">
+    <span class="absolute left-0 bottom-0 z-[-1]">
          <svg
            width="217"
            height="229"
@@ -58,7 +36,7 @@
             </defs>
          </svg>
       </span>
-    <span class="absolute top-10 right-10 z-[-1]">
+    <span class="absolute top-5 right-10 z-[-1]">
          <svg
            width="75"
            height="75"
