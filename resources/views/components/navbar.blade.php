20f5be8e22e92e8bcb3476a1f454540dded1fca7
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-03-06 09:24:19 +0400

    Add YouTube subscribe button to navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,54 +1,67 @@
 <!-- ====== Navbar Section Start -->
 <header
-    x-data="
+  x-data="
         {
           navbarOpen: false
         }
       "
-    class="bg-white w-full flex items-center"
+  class="fixed left-0 top-0 z-50 bg-white w-full flex items-center shadow-md dark:bg-slate-900 h-24"
 >
-    <div class="container">
-        <div class="flex -mx-4 items-center justify-between relative">
-            <div class="px-4 w-60 max-w-full">
-                <a href="/" class="w-full flex items-center py-2">
-                    <img
-                        src="{{ url('/img/logo.png') }}"
-                        alt="logo"
-                        class="w-[64px]"
-                    />
-                    <span class="text-2xl font-bold text-[#0c7187]">TheCodeholic</span>
-                </a>
-            </div>
-            <div class="flex px-4 justify-end items-center w-full">
-                <div>
-                    <x-navbar-hamburger @click="navbarOpen = !navbarOpen" x-bind:class="navbarOpen && 'navbarTogglerActive'"></x-navbar-hamburger>
-                    <nav
-                        :class="!navbarOpen && 'hidden' "
-                        id="navbarCollapse"
-                        class="
+  <div class="container">
+    <div class="flex -mx-4 items-center justify-between relative">
+      <div class="pr-4 w-60 max-w-full">
+        <a href="/" class="w-full flex items-center py-2">
+          <img
+            src="{{ url('/img/logo.png') }}"
+            alt="logo"
+            class="w-[64px] inline-block dark:hidden"
+          />
+          <img
+            src="{{ url('/img/logo-white.png') }}"
+            alt="logo"
+            class="w-[64px] hidden dark:inline-block"
+          />
+          <span class="text-2xl font-bold text-[#0c7187] dark:text-white">TheCodeholic</span>
+        </a>
+      </div>
+      <div class="flex px-4 justify-end items-center w-full">
+        <div>
+          <x-navbar-hamburger @click="navbarOpen = !navbarOpen"
+                              x-bind:class="navbarOpen && 'navbarTogglerActive'"></x-navbar-hamburger>
+          <nav
+            :class="!navbarOpen && 'hidden' "
+            id="navbarCollapse"
+            class="
                           absolute
                           py-5
                           px-6
-                          bg-white
                           shadow
                           rounded-lg
                           max-w-[250px]
                           w-full
-                          lg:max-w-full lg:w-full
+                          lg:max-w-full
+                          lg:w-full
                           right-4
                           top-full
-                          lg:block lg:static lg:shadow-none
+                          lg:block
+                          lg:static
+                          lg:shadow-none
+                          dark:text-gray-300
                         "
-                    >
-                        <ul class="blcok lg:flex">
-                            @foreach($navigationItems as $item)
-                                <x-navbar-item :href="$item['href']">{{ $item['label'] }}</x-navbar-item>
-                            @endforeach
-                        </ul>
-                    </nav>
-                </div>
-            </div>
+          >
+            <ul class="block lg:flex lg:items-center">
+              @foreach($navigationItems as $item)
+                <x-navbar-item :href="$item['href']">{{ $item['label'] }}</x-navbar-item>
+              @endforeach
+
+              <div class="ml-16 relative top-1">
+                <div class="g-ytsubscribe" data-channelid="UC_UMEcP_kF0z4E6KbxCpV1w" data-layout="default" data-count="default"></div>
+              </div>
+            </ul>
+          </nav>
         </div>
+      </div>
     </div>
+  </div>
 </header>
 <!-- ====== Navbar Section End -->
