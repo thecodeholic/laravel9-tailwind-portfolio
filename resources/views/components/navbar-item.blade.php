02067f892e8756a8c9691e75d68a112161442f74
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-03-06 09:25:46 +0400

    Change navbar items text color

--- resources/views/components/navbar-item.blade.php
+++ resources/views/components/navbar-item.blade.php
@@ -1,5 +1,5 @@
 <li>
-    <a href="{{ $href }}" class="text-base font-medium text-dark hover:text-primary py-2 lg:inline-flex flex lg:ml-12">
+    <a href="{{ $href }}" class="text-base font-medium text-dark dark:text-gray-200 hover:text-primary py-2 lg:inline-flex flex lg:ml-12">
         {{ $slot }}
     </a>
 </li>
