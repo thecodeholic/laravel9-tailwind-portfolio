dcb2a3720320096af6a5f4bfedb4938667a75789
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2022-03-19 19:04:59 +0400

    Link portfolio item image into github url

--- resources/views/components/portfolio-item.blade.php
+++ resources/views/components/portfolio-item.blade.php
@@ -4,14 +4,17 @@
   class="w-full md:w-1/2 xl:w-1/3 px-4"
 >
   <div class="relative mb-12">
-    <div class="rounded-lg overflow-hidden">
-      <img
-        src="{{ $image }}"
-        alt="portfolio"
-        class="w-full h-[240px] object-cover"
-      />
-    </div>
-    <div class="text-center bg-white dark:bg-slate-800 relative z-10 py-9 px-3 rounded-lg shadow-lg mx-7 -mt-10 h-[260px]">
+    <a href="{{$github}}" target="_blank">
+      <div class="rounded-lg overflow-hidden ease-in duration-100 hover:scale-105">
+        <img
+          src="{{ $image }}"
+          alt="portfolio"
+          class="w-full h-[260px] object-cover"
+        />
+      </div>
+    </a>
+    <div
+      class="text-center bg-white dark:bg-slate-800 relative z-10 py-9 px-3 rounded-lg shadow-lg mx-7 -mt-4 h-[260px]">
       <span class="text-sm text-primary font-semibold block mb-2">
         {{ implode(", ", $categories) }}
       </span>
@@ -22,7 +25,8 @@
         href="{{$github}}"
         target="_blank"
         class="
-          text-body-color text-sm
+          text-body-color
+          text-sm
           font-semibold
           py-3
           px-7
